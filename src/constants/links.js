5b9fd884e9faafc1af1bfc309bece6f0018a3cc8
willlaptop-PC\willlaptop <wpratt353@gmail.com>
2020-07-03 14:08:30 -0500

    init

---
+++ src/constants/links.js
@@ -0,0 +1,6 @@
+export default [
+  /* {
+    path: '/contact',
+    text: 'Contact',
+  }, */
+]
---
+++ src/styles/theme.js
@@ -0,0 +1,38 @@
+const theme = {
+  colors: {
+    black: '#111111',
+    white: '#ffffff',
+    grey: '#f2f2f2',
+    smoke: '#f4f1ee',
+    yellow: '#e79124',
+    green: '#052c25',
+    pink: '#d5a29e',
+  },
+  fonts: {
+    sans: '"Montserrat", sans-serif',
+    serif: '"Alice", serif',
+  },
+  fontSize: {
+    xlarge: '3rem',
+    large: '4rem',
+    xmedium: '3.5rem',
+    medium: '2rem',
+    small: '1.5rem',
+    xsmall: '0.9rem',
+    xxsmall: '0.7rem',
+  },
+  sizes: {
+    maxWidth: '90vw',
+    maxWidthMedium: '80vw',
+    maxWidthCentered: '70vw',
+    maxWidthSmall: '40vw',
+  },
+  responsive: {
+    small: '576px',
+    medium: '768px',
+    large: '992px',
+    xlarge: '1200px',
+  },
+}
+
+export default theme
