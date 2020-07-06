932f0a2c201dd4d5979c113f6d205394e5c89660
willlaptop-PC\willlaptop <wpratt353@gmail.com>
2020-07-06 13:45:14 -0500

    footer init

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,33 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-import {} from "./style"
+import Logo from '../Header/Logo'
+
+import { FooterWrap } from './style'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <FooterWrap>
+      <Row>
+        <Col md={4}>
+          <Logo />
+        </Col>
+        <Col md={4}>
+          <Link to="#">123 456 7890</Link>
+          <br />
+          <Link to="#">william@howler.com</Link>
+        </Col>
+        <Col className="align-right" md={4}>
+          -
+          <br />
+          <Link to="#">Designed and built by NAME_HERE</Link>
+          <br />© 2020 William Joseph | All Rights Reserved
+        </Col>
+      </Row>
+    </FooterWrap>
+  )
 }
 
 export default Footer
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,5 +1,30 @@
-import styled from "styled-components"
-import Container from "react-bootstrap/Container"
-import Col from "react-bootstrap/Col"
+import styled from 'styled-components'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 
-export const FooterWrap = styled(Container)``
+export const FooterWrap = styled(Container)`
+  padding: 8rem 0 5rem 0;
+  background-color: ${(props) => props.theme.colors.white};
+  color: ${(props) => props.theme.colors.teal};
+  font-size: ${(props) => props.theme.fontSize.small};
+
+  .logo {
+    color: ${(props) => props.theme.colors.teal};
+  }
+  .row {
+    .align-right {
+      font-size: ${(props) => props.theme.fontSize.xsmall};
+      color: ${(props) => props.theme.colors.grey};
+      text-align: right;
+    }
+    .align-right a {
+      color: ${(props) => props.theme.colors.grey};
+      font-size: ${(props) => props.theme.fontSize.xsmall};
+      margin-bottom: ${(props) => props.theme.fontSize.xxsmall};
+
+      :hover {
+        color: ${(props) => props.theme.colors.teal};
+      }
+    }
+  }
+`
