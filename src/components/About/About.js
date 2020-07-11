b35681ac066d28b3d4da7be55dfd49c1d3c6b63e
willlaptop-PC\willlaptop <wpratt353@gmail.com>
2020-07-11 15:09:15 -0500

    about init

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import { StaticQuery, graphql, Link } from 'gatsby'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import BackgroundImage from 'gatsby-background-image'
 
-import { AboutWrap, StyledBackground } from './style'
+import { AboutWrap, StyledCol } from './style'
 
 class About extends Component {
   render() {
@@ -13,26 +15,73 @@ class About extends Component {
       <AboutWrap>
         <Container>
           <Row>
-            <Col sm={12} lg={6}>
-              NU Creative - London
-              <StyledBackground
-                fadeIn
-                Tag="div"
-                className="about-image"
-                fluid={data.about1.childImageSharp.fluid}
-              />
-            </Col>
-            <Col sm={12} lg={6}>
-              Just a COVID screen? How about a long term solution.
-            </Col>
+            <StyledCol sm={12} lg={6}>
+              <div className="small-col">
+                <Link to="#">
+                  <BackgroundImage
+                    fadeIn
+                    Tag="div"
+                    className="about-image small-image"
+                    fluid={data.about1.childImageSharp.fluid}
+                  ></BackgroundImage>
+                  <h4>
+                    NU Creative - London
+                    <span>
+                      <FontAwesomeIcon icon={faPlus} />
+                    </span>
+                  </h4>
+                </Link>
+              </div>
+            </StyledCol>
+            <StyledCol sm={12} lg={6}>
+              <Link to="#">
+                <BackgroundImage
+                  fadeIn
+                  Tag="div"
+                  className="about-image tall-image"
+                  fluid={data.about2.childImageSharp.fluid}
+                ></BackgroundImage>
+                <h4>
+                  Just a COVID screen? How about a long term solution.
+                  <span>
+                    <FontAwesomeIcon icon={faPlus} />
+                  </span>
+                </h4>
+              </Link>
+            </StyledCol>
           </Row>
           <Row>
-            <Col sm={12} lg={6}>
-              test
-            </Col>
-            <Col sm={12} lg={6}>
-              test
-            </Col>
+            <StyledCol sm={12} lg={6}>
+              <Link to="#">
+                <BackgroundImage
+                  fadeIn
+                  Tag="div"
+                  className="about-image mid-image"
+                  fluid={data.about3.childImageSharp.fluid}
+                ></BackgroundImage>
+                <h4>
+                  Hex - A conceptual shelving unit
+                  <span>
+                    <FontAwesomeIcon icon={faPlus} />
+                  </span>
+                </h4>
+              </Link>
+            </StyledCol>
+            <StyledCol className="block-quote" sm={12} lg={6}>
+              <h3>
+                "Exactly what we wanted. Great product and helpful guys, would
+                highly recommend"
+                <br />
+                <br />
+                Summit Campers
+              </h3>
+              <Link to="#">
+                What we do
+                <span>
+                  <FontAwesomeIcon icon={faLongArrowAltRight} />
+                </span>
+              </Link>
+            </StyledCol>
           </Row>
         </Container>
       </AboutWrap>
@@ -46,7 +95,21 @@ export default (props) => (
       query About {
         about1: file(relativePath: { eq: "about/1.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 2500) {
+            fluid(maxWidth: 2500, maxHeight: 1673) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
+          }
+        }
+        about2: file(relativePath: { eq: "about/2.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 2500, maxHeight: 1673) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
+          }
+        }
+        about3: file(relativePath: { eq: "about/3.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 2500, maxHeight: 1673) {
               ...GatsbyImageSharpFluid_noBase64
             }
           }
